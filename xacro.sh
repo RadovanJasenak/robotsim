f25fe9d2b6a50e576a91164fcaa0f5382645b49b
RadovanJasenak <jasenak.radovan@gmail.com>
2022-03-02 20:44:55 +0100

    fix to launching xacro

--- xacro.sh
+++ xacro.sh
@@ -5,8 +5,8 @@ then
 	/opt/ros/noetic/bin/xacro $1
 elif [ -f /usr/bin/docker ]
 	/usr/bin/docker run -it --name ros-sh --net=host -v "$(pwd)":/app osrf/ros:noetic-desktop-full /opt/ros/noetic/bin/xacro /app/$1
+then
+  pass
 else
 	echo "xacro nebolo najdene!"
 fi
-
-
